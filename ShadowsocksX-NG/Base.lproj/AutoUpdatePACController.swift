e93aebb568c00a82db78a580110aaf0e43dc08f5
qinyuhang <qinyuhangxiaoxiang@gmail.com>
2016-08-14 15:44:29 +0800

    add xib view
    add xib view

---
+++ ShadowsocksX-NG/Base.lproj/AutoUpdatePACController.swift
@@ -0,0 +1,19 @@
+//
+//  AutoUpdatePACController.swift
+//  ShadowsocksX-NG
+//
+//  Created by 秦宇航 on 16/8/14.
+//  Copyright © 2016年 qiuyuzhou. All rights reserved.
+//
+
+import Cocoa
+
+class AutoUpdatePACController: NSWindowController {
+
+    override func windowDidLoad() {
+        super.windowDidLoad()
+
+        // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+    }
+    
+}
